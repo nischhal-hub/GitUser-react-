b1e5b53ce67730f3220dfe4dec252df56971be38
nischhal-nub <nischalbh51@gmail.com>
2024-01-03 08:06:15 +0545

    create user card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { GithubContext } from '../context/context';
+import { useGlobalContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = useGlobalContext();
+  const {avatar_url,name,twitter_username,url,company,blog,location} = githubUser;
+  return <>
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+        <h4>{name}</h4>
+        <p>@{twitter_username}</p>
+        </div>
+        <a href={url}>follow</a>
+      </header>
+      <article className='bio'>
+        <ul className="links">
+          <li><p><MdBusiness /> {company}</p></li>
+          <li><p><MdLocationOn /> {location}</p></li>
+          <li><a href={blog}><MdLink /> {blog}</a></li>
+        </ul>
+      </article>
+    </Wrapper>
+  </>;
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
